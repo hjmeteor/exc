003c65a58fb5514fed8c0ed6b6c05ab6abe1f0dc
Meteor <93meteor@gmail.com>
2016-11-17 03:21:14 +0000

    task6,7

---
+++ cw2/task6/run.sh
@@ -0,0 +1,1 @@
+cat /Users/Meteor/OneDrive\ -\ University\ of\ Edinburgh/EXC/Assignment/cw2/data/par3/webSmall.txt | ./extend_reservoir_sampling.py > output.out 
---
+++ cw2/task7/bloom_filter.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from bitarray import bitarray
+import mmh3
+import sys
+import math
+
+
+class BloomFilter:
+    def __init__(self, bitarray_num, hash_num):
+        self.bitarray_num = bitarray_num
+        self.hash_num = hash_num
+        self.bit_array = bitarray(bitarray_num)
+        self.bit_array.setall(0)
+
+    def add(self, string):
+        for seed in xrange(self.hash_num):
+            result = mmh3.hash(string, seed) % self.bitarray_num
+            self.bit_array[result] = 1
+
+    def query(self, string):
+        for seed in xrange(self.hash_num):
+            result = mmh3.hash(string, seed) % self.bitarray_num
+            if self.bit_array[result] == 0:
+                return False
+        return True
+
+
+def get_num(false_positive_probility, entries):
+    bit_p_key = math.ceil(-(math.log(false_positive_probility, 2) / math.log(2)
+                            ))
+    bitarray_num = bit_p_key * float(entries)
+    hash_num = math.ceil(bit_p_key * math.log(2))
+    return (int(bitarray_num), int(hash_num))
+
+
+false_positive_probility = 0.01
+num = get_num(false_positive_probility, sys.argv[1])
+print(num)
+bf = BloomFilter(num[0], num[1])
+for line in sys.stdin:
+    line = line.strip()
+    if bf.query(line) == False:
+        bf.add(line)
+        print("{0}".format(line))
